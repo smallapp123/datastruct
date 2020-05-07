cb1a9bab492bb3d2aa1cab3633f2e4362b347960
cyqlg <example@163.com>
2020-05-07 12:06:08 +0800

    一些代码

---
+++ src/main/java/com/CircleArrayQueue.kt
@@ -0,0 +1,46 @@
+package com
+
+class CircleArrayQueue(private val maxSize: Int) {
+    private var front: Int = -1
+    private var rear: Int = -1
+    private var arr = IntArray(maxSize)
+
+    fun isFull(): Boolean {
+        return rear == maxSize - 1
+    }
+
+    fun isEmpty(): Boolean {
+        return rear == front
+    }
+
+    fun addQueue(item: Int) {
+        if (isFull()) {
+            println("队列已满")
+            return
+        }
+        rear++
+        arr[rear] = item
+    }
+
+    fun getQueue(): Int {
+        if (isEmpty()) {
+            throw RuntimeException("队列已空")
+        }
+        front++
+        return arr[front]
+    }
+
+    fun showQueue() {
+        if (isEmpty()) {
+            println("队列为空")
+        }
+        arr.forEach { println(it) }
+    }
+
+    fun headQueue(): Int {
+        if (isEmpty()) {
+            throw RuntimeException("队列为空")
+        }
+        return arr[front + 1]
+    }
+}
