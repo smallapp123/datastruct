33a669c3bdc046df552f1ea89f730bc7716f4112
cyqlg <example@163.com>
2020-05-07 14:40:52 +0800

    第二次提交

---
+++ src/test/java/com/DemoTest.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.junit.Test;
+
+import java.io.*;
+
+public class DemoTest {
+    @Test
+    public void FileTest() throws Exception {
+        File file = new File("ym.jpg");
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        byte[] buffer = new byte[24];
+        int len=0;
+        BufferedOutputStream bos=new BufferedOutputStream(new FileOutputStream(new File("ym2.jpg")));
+        while ((len=bis.read(buffer)) != -1) {
+            bos.write(buffer, 0, len);
+        }
+        bis.close();
+        bos.close();
+    }
+}
